c1d4467d6d465d36e33dcb6dbd2fbfa6299d7415
mdd317 <michald317@gmail.com>
2023-06-15 15:36:17 +0200

    Adding Employees to the database with controller and views

---
+++ WebApplication1/Controllers/EmployeesController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Employees.Include(e => e.Cinemas);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Employees/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Employees == null)
+            {
+                return NotFound();
+            }
+
+            var employees = await _context.Employees
+                .Include(e => e.Cinemas)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employees == null)
+            {
+                return NotFound();
+            }
+
+            return View(employees);
+        }
+
+        // GET: Employees/Create
+        public IActionResult Create()
+        {
+            ViewData["CinemaId"] = new SelectList(_context.Cinemas, "Id", "Description");
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,SurName,CinemaId")] Employees employees)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(employees);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CinemaId"] = new SelectList(_context.Cinemas, "Id", "Description", employees.CinemaId);
+            return View(employees);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Employees == null)
+            {
+                return NotFound();
+            }
+
+            var employees = await _context.Employees.FindAsync(id);
+            if (employees == null)
+            {
+                return NotFound();
+            }
+            ViewData["CinemaId"] = new SelectList(_context.Cinemas, "Id", "Description", employees.CinemaId);
+            return View(employees);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,SurName,CinemaId")] Employees employees)
+        {
+            if (id != employees.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employees);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeesExists(employees.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CinemaId"] = new SelectList(_context.Cinemas, "Id", "Description", employees.CinemaId);
+            return View(employees);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Employees == null)
+            {
+                return NotFound();
+            }
+
+            var employees = await _context.Employees
+                .Include(e => e.Cinemas)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employees == null)
+            {
+                return NotFound();
+            }
+
+            return View(employees);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Employees == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Employees'  is null.");
+            }
+            var employees = await _context.Employees.FindAsync(id);
+            if (employees != null)
+            {
+                _context.Employees.Remove(employees);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeesExists(int id)
+        {
+          return (_context.Employees?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ WebApplication1/Models/Employees.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace WebApplication1.Models
+
+{
+    public class Employees
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string SurName { get; set; }
+
+
+        // Relationships
+        public virtual Cinema Cinemas { get; set; }
+
+        public int CinemaId { get; set; }
+
+
+    }
+}
